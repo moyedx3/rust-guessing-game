aadad81c795be7f4319e96751a86eaf5c0a9f35f
moyedx3 <samoyedali@gmail.com>
2024-08-22 23:54:05 +0900

    mamamia

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,30 @@ use rand::Rng;
 use std::{cmp::Ordering, io};
 
 fn main() {
-    //let _first = String::from("Rust"); instance of String, use when you dont know the value of the string ahead of the time
-    let correct = rand::thread_rng().gen_range(1..=10);
-    //println!("(correct: {})", correct);
-    println!("Hey,guess a number 1-10:");
+    let mut how_many = String::new();
+    println!("How many times do you want to guess?");
+    io::stdin()
+        .read_line(&mut how_many)
+        .expect("Failed to read line");
 
-    loop {
+    let num_guesses: u8 = how_many
+        .trim()
+        .parse()
+        .expect("Please enter a valid number.");
+
+    let mut correct = Vec::new();
+
+    for _ in 0..num_guesses {
+        correct.push(rand::thread_rng().gen_range(1..=10));
+    }
+
+    //println!("{correct:?}");
+
+    let mut guesses_made = 0;
+
+    println!("Please enter your guess.");
+
+    while guesses_made < num_guesses {
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -21,14 +39,22 @@ fn main() {
             }
         };
 
-        match guess.cmp(&correct) {
+        match guess.cmp(&correct[guesses_made as usize]) {
             Ordering::Greater => println!("You guessed too high."),
             Ordering::Less => println!("You guessed too low!"),
             Ordering::Equal => {
                 println!("You guessed the correct number.");
-                break;
+                guesses_made += 1;
+                if guesses_made < num_guesses {
+                    println!("Please enter your next guess.");
+                }
             }
         };
+    }
+    println!("You have guessed all the numbers correctly. The correct answers were:");
+
+    for item in correct {
+        println!("{item}");
     }
 }
 
